35eefee33926ee3bfbc48e8a39fe29e68ac377d6
Eric Nwogbo <ericis2007@gmail.com>
2024-12-08 14:11:56 +0100

    Fixed dieletric material

--- src/materials/dielectirc.rs
+++ src/materials/dielectirc.rs
@@ -14,15 +14,15 @@ impl Dielectirc {
 
 impl Material for Dielectirc {
     fn scatter(&self, ray: &Ray, hit: &HitInfo) -> Option<ScatterInfo> {
-        let idx = if hit.front_face { self.refraction_index } else { 1.0 / self.refraction_index };
+        let idx = if !hit.front_face { self.refraction_index } else { 1.0 / self.refraction_index };
         let unit_dir = ray.dir.normalized();
-        let cos_theta = Vec3::dot(&unit_dir, &hit.normal).min(1.0);
+        let cos_theta = Vec3::dot(&unit_dir.negated(), &hit.normal).min(1.0);
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
         Some(ScatterInfo {
             ray: Ray::new(hit.position.clone(), if idx * sin_theta > 1.0 {
-                ray.dir.reflect(&hit.normal)
+                    unit_dir.reflect(&hit.normal)
                 } else {
-                    ray.dir.refract(&hit.normal, idx)
+                    unit_dir.refract(&hit.normal, idx)
                 }),
             atten: (1.0, 1.0, 1.0).into(),
         })
